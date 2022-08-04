b4f41960ac11ebca90ed2f19a1cf93c3fa3af92d
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-04 16:58:46 +0530

    Meal Page

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Quick Bite</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Food Being Served" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/MealsList/AvaliableMeals.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+import Card from "../../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+  {
+    id: "m5",
+    name: "Garlic Bread",
+    description: "France, bread",
+    price: 5.5,
+  },
+  {
+    id: "m6",
+    name: "Ramen",
+    description: "Chinse, Noodle soup",
+    price: 15.99,
+  },
+];
+
+function AvailableMeans() {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+}
+
+export default AvailableMeans;
